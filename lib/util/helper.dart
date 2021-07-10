9e186a9a909f72015212dda4041abada33118764
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-07-10 18:20:10 +0300

    server url was moved to constants

--- lib/util/helper.dart
+++ lib/util/helper.dart
@@ -2,6 +2,8 @@ import 'package:http/http.dart' as http;
 
 import 'holder.dart';
 
+const String serverUrl = 'http://37.148.212.195:5000';
+
 Future<http.Response> postCall(String service, Object? body, bool hasToken) async {
   final fixedHeaders = {
     "Access-Control-Allow-Origin": "*",
@@ -11,7 +13,7 @@ Future<http.Response> postCall(String service, Object? body, bool hasToken) asyn
     var token = Holder.authToken;
     fixedHeaders["Authorization"] = 'Bearer $token';
   }
-  return await http.post(Uri.parse('http://37.148.212.195:5000/$service'), headers: fixedHeaders, body: body);
+  return await http.post(Uri.parse('$serverUrl/$service'), headers: fixedHeaders, body: body);
 }
 
 Future<http.Response> getCall(String service, bool hasToken) async {
@@ -23,5 +25,5 @@ Future<http.Response> getCall(String service, bool hasToken) async {
     var token = Holder.authToken;
     fixedHeaders["Authorization"] = 'Bearer $token';
   }
-  return await http.get(Uri.parse('http://37.148.212.195:5000/$service'), headers: fixedHeaders);
+  return await http.get(Uri.parse('$serverUrl/$service'), headers: fixedHeaders);
 }
